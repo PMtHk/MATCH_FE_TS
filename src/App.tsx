6a3534fc845ee538adb0ed8ba8d70ae61879d582
zooby <zooby88@naver.com>
2023-05-27 15:38:52 +0900

    feat : add landingpage navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import Landing from 'pages/Landing';
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Landing />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Landing.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from 'components/landing/Navbar';
+
+function Landing() {
+  return (
+    <>
+      <Navbar />
+      <h1>Landing</h1>
+    </>
+  );
+}
+
+export default Landing;
