8a54c5ab5cb0cba1cff317256a5eb0e2383e45b7
zooby <zooby88@naver.com>
2023-05-30 17:34:40 +0900

    feat : add signin route

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LandingPage from 'pages/LandingPage';
+import SignInPage from 'pages/SignInPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<SignInPage />} />
       </Routes>
     </BrowserRouter>
   );
