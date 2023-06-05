a94bec5333feeb600508efb7956858c856555811
zooby <zooby88@naver.com>
2023-06-06 00:56:56 +0900

    feat : add index.css to disable react-error-overlay

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import { PersistGate } from 'redux-persist/integration/react';
 // mui
 import { CssBaseline } from '@mui/material';
 
+// index.css
+import './index.css';
+
 import App from './App';
 
 import store from './store/index';
