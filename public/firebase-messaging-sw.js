430bee16f844ab6b28dabd9d7a4d47c8b593cb44
zooby <zooby88@naver.com>
2023-07-30 19:09:24 +0900

    feat: add background notification with contents

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,13 +18,42 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
+const games = {
+  lol: '리그오브레전드',
+  overwatch: '오버워치',
+  pubg: '배틀그라운드',
+  valorant: '발로란트',
+};
+
 messaging.onBackgroundMessage((payload) => {
   console.log('Received background message ', payload);
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = `${payload.data.createdBy}의 방(${
+    games[payload.data.game]
+  })`;
   const notificationOptions = {
-    body: payload.notification.body,
-    icon: './logo192.png',
+    body: payload.data.content,
+    icon: './logo512.png',
+    content: payload.data.content,
+    timestamp: payload.data.timestamp,
+    user_nickname: payload.data.user_nickname,
+    roomId: payload.data.roomId,
+    createdBy: payload.data.createdBy,
+    game: payload.data.game,
   };
+
+  self.addEventListener('notificationclick', (event) => {
+    let clickResponsePromise = Promise.resolve();
+
+    console.log(event.notification.data);
+    console.log(event);
+    event.notification.close();
+
+    clickResponsePromise = clients.openWindow(
+      `https://match-gg.kr/${payload.data.game}/${payload.data.roomId}`,
+    );
+
+    event.waitUntil(Promise.all([clickResponsePromise]));
+  });
 
   return self.registration.showNotification(
     notificationTitle,
