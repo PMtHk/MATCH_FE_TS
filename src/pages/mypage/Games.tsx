5d1cd2a999ef5cfaae83930c796f1483562429a8
Minggurazi <jhsong1450@naver.com>
2023-08-16 00:35:19 +0900

    feat : 연결된 게임의 정보를 보여줄 UI 및 기능 구현

--- src/pages/mypage/Games.tsx
+++ src/pages/mypage/Games.tsx
@@ -1,29 +1,47 @@
 import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 // mui
 import { styled } from '@mui/system';
 import MuiBox from '@mui/material/Box';
 import MuiTypography from '@mui/material/Typography';
-import MuiTextField from '@mui/material/TextField';
-import MuiButton from '@mui/material/Button';
-import MuiToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import MuiToggleButton from '@mui/material/ToggleButton';
-import MuiSelect, { SelectChangeEvent } from '@mui/material/Select';
-import MuiMenuItem from '@mui/material/MenuItem';
+import {
+  fetchSummonerInfo,
+  loadSummonerInfoIntoDB,
+} from 'apis/api/leagueoflegends';
+import {
+  checkPUBGUserPlatform,
+  fetchPubgPlayerInfo,
+  loadPubgPlayerInfoIntoDB,
+} from 'apis/api/pubg';
+import { fetchPlayerInfo, loadOWPlayerInfoInDB } from 'apis/api/overwatch';
+import Circular from 'components/loading/Circular';
+import GradeIcon from '@mui/icons-material/Grade';
 
-import MuiOutlinedInput from '@mui/material/OutlinedInput';
 import { RootState } from 'store';
-import { Button, ButtonGroup, Divider } from '@mui/material';
+import { Button, Divider, Tooltip } from '@mui/material';
 import GameIcon from 'components/GameIcon';
 import { gameList } from 'assets/Games.data';
+import { snackbarActions } from 'store/snackbar-slice';
+
+import { userActions } from 'store/user-slice';
+import { handleRepresentativeGame } from 'apis/api/user';
+import LolInfo from './Games/LolInfo';
+import PubgInfo from './Games/PubgInfo';
+import OverwatchInfo from './Games/OverwatchInfo';
+import AddGame from './Games/AddGame';
 
 type GameFilterProps = {
   selectedGame: string;
   setSelectedGame: Dispatch<SetStateAction<string>>;
+  representative: string;
 };
 
-const GameFilterBar = ({ selectedGame, setSelectedGame }: GameFilterProps) => {
+const GameFilterBar = ({
+  selectedGame,
+  setSelectedGame,
+  representative,
+}: GameFilterProps) => {
   return (
     <>
       <GameSelector>
@@ -45,6 +63,11 @@ const GameFilterBar = ({ selectedGame, setSelectedGame }: GameFilterProps) => {
               <GameTypo selected={selectedGame === aGame.id}>
                 {aGame.name_kor}
               </GameTypo>
+              {aGame.id === representative && (
+                <Tooltip title={`현재 대표게임 : ${representative}`}>
+                  <GradeIcon sx={{ color: '#ffc939' }} />
+                </Tooltip>
+              )}
             </GameSelectorItem>
           );
         })}
@@ -54,60 +77,82 @@ const GameFilterBar = ({ selectedGame, setSelectedGame }: GameFilterProps) => {
   );
 };
 
-type GameProps = {
-  label: string;
-  value: string;
-};
+const GameDataUpdateButton = ({ game }: any) => {
+  const dispatch = useDispatch();
 
-const Game = ({ label, value }: GameProps) => {
-  const nickname = useSelector(
-    (state: RootState) =>
-      state.user.games[value as 'lol' | 'pubg' | 'overwatch'],
-  );
+  const { games } = useSelector((state: RootState) => state.user);
 
-  const [nicknameInput, setNicknameInput] = useState<string>('');
-  useEffect(() => {
-    setNicknameInput(nickname);
-  }, [nickname]);
-
-  const [isEdit, setIsEdit] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNicknameInput(e.target.value);
-  };
-
-  const handleEdit = () => {
-    setIsEdit(!isEdit);
-  };
-
-  const handleCertify = () => {
+  const handleUpdate = async () => {
+    const nickname = games[game as 'lol' | 'pubg' | 'overwatch'];
     setIsLoading(true);
-    // 서버에서 인증 받는 API 호출
-    // 이후 유저 정보 수정 요청 API 호출
+    if (game === 'lol') await loadSummonerInfoIntoDB(nickname);
+    if (game === 'pubg') {
+      const response = await checkPUBGUserPlatform(nickname);
+      if (response.platform && response.nickname) {
+        await loadPubgPlayerInfoIntoDB(response.nickname, response.platform);
+      }
+    }
+    if (game === 'overwatch') {
+      const [name, battleTag] = nickname.split('#');
+      await loadOWPlayerInfoInDB(name, battleTag);
+    }
     setIsLoading(false);
+    dispatch(
+      snackbarActions.OPEN_SNACKBAR({
+        message: '정보 갱신이 완료되었습니다.',
+        severity: 'success',
+      }),
+    );
   };
 
   return (
-    <Section>
-      <SectionTitle>{label}</SectionTitle>
-      <InputWrapper>
-        <NicknameTextField
-          value={nicknameInput || ''}
-          disabled={!isEdit}
-          size="small"
-          onChange={handleInput}
-          endAdornment={
-            isEdit && (
-              <CertifyButton onClick={handleCertify}>인증하기</CertifyButton>
-            )
-          }
-        />
-        <ControlButton onClick={handleEdit}>
-          {isEdit ? '취소하기' : '변경하기'}
-        </ControlButton>
-      </InputWrapper>
-    </Section>
+    <Button
+      fullWidth
+      variant="outlined"
+      sx={{ marginRight: '12px' }}
+      onClick={handleUpdate}
+      disabled={isLoading}
+    >
+      {isLoading ? '갱신중...' : '정보 갱신하기'}
+    </Button>
+  );
+};
+
+const HandleRepresentativeButton = ({ game, representative }: any) => {
+  const dispatch = useDispatch();
+  const handleRepresentative = async () => {
+    const response = await handleRepresentativeGame(game);
+    if (response) {
+      // 대표게임 변경 성공
+      dispatch(userActions.SET_REPRESENTATIVE({ representative: game }));
+      dispatch(
+        snackbarActions.OPEN_SNACKBAR({
+          message: '대표게임 변경이 완료되었습니다.',
+          severity: 'success',
+        }),
+      );
+    }
+    // 대표게임 변경 실패
+    else {
+      dispatch(
+        snackbarActions.OPEN_SNACKBAR({
+          message: '대표게임 변경에 실패하였습니다.',
+          severity: 'error',
+        }),
+      );
+    }
+  };
+  return (
+    <UpdateButton
+      fullWidth
+      onClick={handleRepresentative}
+      variant="outlined"
+      disabled={game === representative}
+    >
+      이 게임으로 대표게임을 변경하기
+    </UpdateButton>
   );
 };
 
@@ -115,20 +160,109 @@ const Games = () => {
   const { representative } = useSelector((state: RootState) => state.user);
   const [selectedGame, setSelectedGame] = useState<string>(representative);
 
-  return (
-    <>
-      <GameFilterBar
-        selectedGame={selectedGame}
-        setSelectedGame={setSelectedGame}
-      />
-      {gameList.map((game) => (
-        <Game key={game.id} label={game.name_kor} value={game.id} />
-      ))}
-    </>
-  );
-};
+  const { games } = useSelector((state: RootState) => state.user);
 
+  const [lolData, setLolData] = useState<object | null>(null);
+  const [pubgData, setPubgData] = useState<object | null>(null);
+  const [overwatchData, setOverwatchData] = useState<object | null>(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const getDatas = async () => {
+      if (games.lol) {
+        const duoRankInfo = await fetchSummonerInfo(games.lol, 'DUO_RANK');
+        const freeRankInfo = await fetchSummonerInfo(games.lol, 'FREE_RANK');
+        setLolData({ duoRankInfo, freeRankInfo });
+      }
+      if (games.pubg) {
+        const { nickname, platform } = await checkPUBGUserPlatform(games.pubg);
+
+        const duoInfo = await fetchPubgPlayerInfo(nickname, platform, 'DUO');
+        const squadInfo = await fetchPubgPlayerInfo(
+          nickname,
+          platform,
+          'SQUAD',
+        );
+        const rankedSquadInfo = await fetchPubgPlayerInfo(
+          nickname,
+          platform,
+          'RANKED_SQUAD',
+        );
+        setPubgData({ duoInfo, squadInfo, rankedSquadInfo });
+      }
+      if (games.overwatch) {
+        const [name, battleTag] = games.overwatch.split('#');
+
+        const rankInfo = await fetchPlayerInfo(name, battleTag, 'RANKED');
+        const normalInfo = await fetchPlayerInfo(name, battleTag, 'NORMAL');
+        setOverwatchData({ rankInfo, normalInfo });
+      }
+      setIsLoading(false);
+    };
+    getDatas();
+  }, [games]);
+
+  if (!isLoading) {
+    return (
+      <Container sx={{ height: '100%' }}>
+        <MenuTitle>연결한 게임</MenuTitle>
+        <GameFilterBar
+          selectedGame={selectedGame}
+          setSelectedGame={setSelectedGame}
+          representative={representative}
+        />
+        {
+          {
+            lol: games.lol ? (
+              <LolInfo data={lolData} />
+            ) : (
+              <AddGame game="lol" />
+            ),
+            pubg: games.pubg ? (
+              <PubgInfo data={pubgData} />
+            ) : (
+              <AddGame game="pubg" />
+            ),
+            overwatch: games.overwatch ? (
+              <OverwatchInfo data={overwatchData} />
+            ) : (
+              <AddGame game="overwatch" />
+            ),
+          }[selectedGame]
+        }
+        {games[selectedGame as 'lol' | 'pubg' | 'overwatch'] && (
+          <ButtonSection>
+            <GameDataUpdateButton game={selectedGame} />
+            <HandleRepresentativeButton
+              game={selectedGame}
+              representative={representative}
+            />
+          </ButtonSection>
+        )}
+      </Container>
+    );
+  }
+  return <Circular height="500px" text="게임 정보를 가져오는 중입니다." />;
+};
 export default Games;
+
+const Container = styled(MuiBox)(() => ({
+  height: '100%',
+})) as typeof MuiBox;
+
+const ButtonSection = styled(MuiBox)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'center',
+})) as typeof MuiBox;
+
+const UpdateButton = styled(Button)(() => ({
+  whiteSpace: 'nowrap',
+  marginRight: '12px',
+})) as typeof Button;
 
 const GameSelector = styled(MuiBox)(({ theme }) => ({
   width: '100%',
@@ -138,6 +272,7 @@ const GameSelector = styled(MuiBox)(({ theme }) => ({
   justifyContent: 'space-around',
   gap: '6px',
   padding: '0 4px 0 4px',
+  marginTop: '12px',
 })) as typeof MuiBox;
 
 interface GameSelectorItem {
@@ -161,20 +296,6 @@ const GameSelectorItem = styled(MuiBox, {
   },
 }));
 
-const HeaderTypo = styled(MuiTypography)(({ theme }) => ({
-  padding: '8px',
-  fontSize: '15px',
-  fontWeight: 'bold',
-  color: theme.palette.primary.main,
-})) as typeof MuiTypography;
-
-const NoPartyTypo = styled(MuiTypography)(({ theme }) => ({
-  padding: '8px',
-  fontSize: '14px',
-  fontWeight: 'bold',
-  color: theme.palette.primary.main,
-})) as typeof MuiTypography;
-
 interface GameTypoProps {
   selected: boolean;
 }
@@ -187,102 +308,7 @@ const GameTypo = styled(MuiTypography, {
   color: selected ? '' : 'grey',
 }));
 
-const GameFilterTypo = styled(MuiTypography)(() => ({
-  fontSize: '24px',
-}));
-
-const GameFilterBarWrapper = styled(MuiBox)(() => ({
-  width: '50%',
-  padding: '12px',
-  display: 'flex',
-  flexDirection: 'row',
-}));
-
-const Container = styled(MuiBox)(({ theme }) => ({
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  justifyContent: 'flex-start',
-  gap: '16px',
-}));
-
 const MenuTitle = styled(MuiTypography)(() => ({
-  width: '100%',
-  borderBottom: '1px solid #969393',
-  fontSize: '22px',
-  fontWeight: '700',
-  padding: '0 0 0 8px',
-})) as typeof MuiTypography;
-
-const Section = styled(MuiBox)(({ theme }) => ({
-  marginBottom: '16px',
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'flex-start',
-  justifyContent: 'center',
-  padding: '8px 0 0 16px',
-  gap: '8px',
-}));
-
-const SectionTitle = styled(MuiTypography)(() => ({
   fontSize: '18px',
   fontWeight: 'bold',
-  marginBottom: '4px',
 }));
-
-const SectionContentTypo = styled(MuiTypography)(() => ({
-  fontSize: '16px',
-  fontWeight: '500',
-  padding: '0 0 0 16px',
-}));
-
-const NicknameTextField = styled(MuiOutlinedInput)(() => ({
-  maxWidth: '280px',
-  minWidth: '280px',
-  '& .MuiInputBase-root': {
-    height: '40px',
-    padding: '0',
-  },
-}));
-
-const CertifyButton = styled(MuiButton)(() => ({
-  height: '40px',
-  width: '80px',
-  fontSize: '14px',
-  fontWeight: 'bold',
-  borderRadius: '0',
-  padding: '0',
-})) as typeof MuiButton;
-
-const InputWrapper = styled(MuiBox)(({ theme }) => ({
-  width: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'flex-start',
-  padding: '0 8px 0 0',
-  gap: '4px',
-})) as typeof MuiBox;
-
-const ControlButton = styled(MuiButton)(() => ({
-  height: '40px',
-  width: '80px',
-  fontSize: '14px',
-  fontWeight: 'bold',
-  borderRadius: '0',
-  padding: '0',
-})) as typeof MuiButton;
-
-const PubgPlatformToggle = styled(MuiToggleButtonGroup)(() => ({
-  '& .MuiToggleButton-root': {
-    height: '40px',
-    fontSize: '14px',
-    fontWeight: 'bold',
-    borderRadius: '4px',
-    padding: '0 8px',
-  },
-})) as typeof MuiToggleButtonGroup;
-
-const Platform = styled(MuiToggleButton)(() => ({})) as typeof MuiToggleButton;
