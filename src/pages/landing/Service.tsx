f24d4c054a2e8c0d6bea207bcdc1e8927b48865f
zooby <zooby88@naver.com>
2023-08-22 22:31:17 +0900

    faet : update services in landing

--- src/pages/landing/Service.tsx
+++ src/pages/landing/Service.tsx
@@ -1,65 +1,10 @@
 import React from 'react';
 
 // mui
+import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
-
-// mui styled components
-import { styled } from '@mui/system';
-
-const Wrapper = styled(Box)(() => ({
-  width: '100%',
-  backgroundColor: '#ffffff',
-})) as typeof Box;
-
-const ContentWrapper = styled(Box)(({ theme }) => ({
-  width: '100%',
-  padding: '200px 0',
-  display: 'flex',
-  flexDirection: 'column',
-  [theme.breakpoints.up('md')]: {
-    flexDirection: 'row',
-  },
-})) as typeof Box;
-
-const ColumnBox = styled(Box)(({ theme }) => ({
-  width: '100%',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'flex-start',
-  alignItems: 'flex-start',
-  padding: '25px 40px',
-  [theme.breakpoints.up('md')]: {
-    width: '50%',
-    padding: '15px 10px',
-  },
-})) as typeof Box;
-
-const ColumnTitle = styled(Typography)(({ theme }) => ({
-  fontSize: 20,
-  fontWeight: 600,
-  color: '#3182f6',
-  padding: '0 0 40px',
-  [theme.breakpoints.up('sm')]: {
-    fontSize: 24,
-  },
-  [theme.breakpoints.up('md')]: {
-    fontSize: 28,
-  },
-})) as typeof Typography;
-
-const ColumnDescription = styled(Typography)(({ theme }) => ({
-  fontSize: 16,
-  fontWeight: 500,
-  padding: '0 0 30px',
-  [theme.breakpoints.up('sm')]: {
-    fontSize: 20,
-  },
-  [theme.breakpoints.up('md')]: {
-    fontSize: 22,
-  },
-})) as typeof Typography;
 
 const Service = () => {
   return (
@@ -67,27 +12,39 @@ const Service = () => {
       <Container maxWidth="lg">
         <ContentWrapper>
           <ColumnBox>
-            <ColumnTitle>파티 찾기</ColumnTitle>
-            <ColumnDescription>
-              카드형 UI 를 통해
-              <br />
-              현재 모집중인 파티들을
-              <br /> 한눈에 확인할 수 있어요.
-              <br />
-              <br />
-              팔로우한 유저들의 새 게시글을
-              <br /> 상단에서 먼저 확인할 수 있어요.
-            </ColumnDescription>
-            {/* image should be here ( Card UI and Party Modal ) */}
+            <ColumnText>
+              <ColumnTitle>파티 찾기</ColumnTitle>
+              <ColumnDescription>
+                인게임 데이터,
+                <br />
+                모집현황을,
+                <br /> 카드형 UI를 통해
+                <br /> 한눈에
+              </ColumnDescription>
+            </ColumnText>
+            <CardUIImageWrapper>
+              <img
+                src="https://d18ghgbbpc0qi2.cloudfront.net/assets/landing/CardUI.png"
+                alt="card_design"
+              />
+            </CardUIImageWrapper>
+            <UserUIImageWrapper>
+              <img
+                src="https://d18ghgbbpc0qi2.cloudfront.net/assets/landing/UserUI.png"
+                alt="usercard_design"
+              />
+            </UserUIImageWrapper>
           </ColumnBox>
           <ColumnBox>
-            <ColumnTitle>채팅</ColumnTitle>
-            <ColumnDescription>
-              파티에 참가하여 대화를 나눠보세요.
-              <br />
-              게임에 앞서, 상대방과 합을 맞출 수 있어요.
-            </ColumnDescription>
-            {/* image should be here ( chatroom UI ) */}
+            <ColumnText>
+              <ColumnTitle>채팅</ColumnTitle>
+              <ColumnDescription>
+                팀원과의 합,
+                <br />
+                파티 전용 채팅을 통해
+                <br /> 미리 맞춰보기
+              </ColumnDescription>
+            </ColumnText>
           </ColumnBox>
         </ContentWrapper>
       </Container>
@@ -96,3 +53,85 @@ const Service = () => {
 };
 
 export default Service;
+
+const Wrapper = styled(Box)(() => ({
+  width: '100%',
+  backgroundColor: '#ffffff',
+})) as typeof Box;
+
+const ContentWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  padding: '100px 0 150px 0',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '160px',
+})) as typeof Box;
+
+const ColumnBox = styled(Box)(({ theme }) => ({
+  width: '100%',
+  position: 'relative',
+  padding: '25px 40px',
+  [theme.breakpoints.up('md')]: {
+    padding: '15px 10px',
+  },
+})) as typeof Box;
+
+const ColumnText = styled(Box)(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+  alignItems: 'flex-start',
+})) as typeof Box;
+
+const ColumnTitle = styled(Typography)(({ theme }) => ({
+  fontSize: 18,
+  fontWeight: 500,
+  color: '#3182f6',
+  padding: '0 0 30px',
+  [theme.breakpoints.up('md')]: {
+    fontSize: 28,
+  },
+})) as typeof Typography;
+
+const ColumnDescription = styled(Typography)(({ theme }) => ({
+  fontSize: 28,
+  fontWeight: 600,
+  padding: '0 0 30px',
+  [theme.breakpoints.up('md')]: {
+    fontSize: 50,
+  },
+})) as typeof Typography;
+
+const CardUIImageWrapper = styled(Box)(({ theme }) => ({
+  '& > img': {
+    width: '100%',
+    cover: 'object-fit',
+    maxWidth: '500px',
+  },
+  [theme.breakpoints.up('md')]: {
+    position: 'absolute',
+    top: '70px',
+    right: '0px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    position: 'absolute',
+    top: '70px',
+    right: '30px',
+  },
+}));
+
+const UserUIImageWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  '& > img': {
+    width: '100%',
+    cover: 'object-fit',
+    maxWidth: '700px',
+  },
+  [theme.breakpoints.up('md')]: {
+    justifyContent: 'flex-end',
+  },
+}));
