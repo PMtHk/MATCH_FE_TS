57c88882740df6ab6c7c5b7aba80d28201625cd4
Minggurazi <jhsong1450@naver.com>
2023-09-14 16:48:51 +0900

    fix : modify platform get api

--- src/apis/api/pubg.ts
+++ src/apis/api/pubg.ts
@@ -20,21 +20,37 @@ import { pubgAxios, defaultAxios } from 'apis/utils';
  * 매치 히스토리가 없어 플랫폼을 구분할 수 없는 경우 에러를 반환한다.
  */
 export const getPlatform = async (nickname: string) => {
+  // steam, kakao response로 platform 계산
+  const calcPlatform = (steamRes: any, kakaoRes: any) => {
+    if (Object.values(steamRes).find((g: any) => g.wins > 0 || g.losses > 0)) {
+      return 'STEAM';
+    }
+    if (Object.values(kakaoRes).find((g: any) => g.wins > 0 || g.losses > 0)) {
+      return 'KAKAO';
+    }
+    return 0;
+  };
+
   try {
+    // accountId 가져오기
+    const accountId: any = await pubgAxios
+      .get(`shards/steam/players?filter[playerNames]=${nickname}`)
+      .then((res) => res.data.data[0].id);
+    console.log(`accountId : ${accountId}`);
+
     // 스팀 요청
     const steamResponse: any = await pubgAxios
-      .get(`shards/steam/players?filter[playerNames]=${nickname}`)
-      .then((res) => res.data.data[0].relationships.matches.data);
+      .get(`shards/steam/players/${accountId}/seasons/lifetime`)
+      .then((res) => res.data.data.attributes.gameModeStats);
 
     // 카카오 요청
     const kakaoResponse: any = await pubgAxios
-      .get(`shards/kakao/players?filter[playerNames]=${nickname}`)
-      .then((res) => res.data.data[0].relationships.matches.data);
+      .get(`shards/kakao/players/${accountId}/seasons/lifetime`)
+      .then((res) => res.data.data.attributes.gameModeStats);
 
-    const platform =
-      steamResponse.length > kakaoResponse.length ? 'STEAM' : 'KAKAO';
+    const platform = calcPlatform(steamResponse, kakaoResponse);
 
-    if (steamResponse.length === kakaoResponse.length) {
+    if (platform === 0) {
       throw new Error(
         '매치 히스토리가 부족해 플레이어 전적 및 플랫폼 정보를 가져올 수 없습니다.',
       );
